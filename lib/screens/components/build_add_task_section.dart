cf6da8177d24e6d211b7453d985f9e9433e7efc4
skaur48351 <skaur48351@gmail.com>
2025-12-10 22:16:58 +0000

    Update

---
+++ lib/screens/components/build_add_task_section.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Widget buildAddTaskSection(nameController, Function(String) addTask) {
+  return Container(
+  decoration: BoxDecoration(color: Colors.white),
+  child: Row(
+    children: [
+
+  ],
+  ),
+  );
+}
